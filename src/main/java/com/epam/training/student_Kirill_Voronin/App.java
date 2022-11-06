77199a982af1416b7d23574ba39c70162b29a780
vorona2441 <vorona2441@gmail.com>
2022-11-06 07:22:48 +0300

    Initialize

---
+++ src/main/java/com/epam/training/student_Kirill_Voronin/App.java
@@ -0,0 +1,13 @@
+package com.epam.training.student_Kirill_Voronin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
